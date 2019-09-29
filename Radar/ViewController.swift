8e6744b7af63a44438bd8c5351221b999dc7c664
Charel Felten <charel.felten@gmail.com>
2019-09-29 09:22:27 +0200

    init

---
+++ Radar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Radar
+//
+//  Created by Charel FELTEN on 29/09/2019.
+//  Copyright © 2019 Charel FELTEN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
