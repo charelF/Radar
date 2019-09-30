3c515836b7dbaa8e16dfcb29299a4ddc1355c113
Charel Felten <charel.felten@gmail.com>
2019-09-30 10:23:38 +0200

    working on tab bar controller

---
+++ Radar/Activity.swift
@@ -0,0 +1,38 @@
+//
+//  Activity.swift
+//  Radar
+//
+//  Created by Charel FELTEN on 30/09/2019.
+//  Copyright © 2019 Charel FELTEN. All rights reserved.
+//
+
+import Foundation
+
+//class Activity {
+//
+//    let name: String
+//    let description: String
+//
+//    let languages: [String]
+//
+//    let type: String
+//    let information: [String:Any]
+//
+//    let ID: Int
+//    let userID: Int
+//
+//    let lat: Double
+//    let long: Double
+//
+//    let creationTime: Double
+//    let activityTime: Double
+//
+//    init(name: String,
+//         description: String,
+//         languages: [String:Any],
+//         type: String,
+//         information: [String:Any],
+//        ) {
+//        <#statements#>
+//    }
+//}
