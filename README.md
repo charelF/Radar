cea9d88cbe35a746474cbb7fc27aaf367137d15b
Charel Felten <charel.felten@gmail.com>
2019-10-08 22:09:46 +0200

    added custom view xib

--- README.md
+++ README.md
@@ -12,6 +12,14 @@ todo:
 - explore which swift file is the best to store the activity arrays? Right now we can store them in activity.swift in a static function, but how to do it latter on? One class that handles requests maybe and stores an activity array that can be accessed by the different view controllers?
 
 
+08/10/19
+we implemented custom view with this tutorial: https://medium.com/better-programming/swift-3-creating-a-custom-view-from-a-xib-ecdfe5b3a960
+todo:
+- do a custom callout by desigining the view in the activityview.xib and then implementing that. tutorials: 
+    - https://sweettutos.com/2016/03/16/how-to-completely-customise-your-map-annotations-callout-views/ (see example application we downloaded)
+    - https://www.raywenderlich.com/548-mapkit-tutorial-getting-started
+
+
 
 
 
---
+++ Radar/ActivityView.swift
@@ -0,0 +1,38 @@
+//
+//  ActivityView.swift
+//  Radar
+//
+//  Created by Charel FELTEN on 08/10/2019.
+//  Copyright © 2019 Charel FELTEN. All rights reserved.
+//
+
+// this uiview is responsable to manage the ActivityView.xib file
+// tutorial from: https://medium.com/better-programming/swift-3-creating-a-custom-view-from-a-xib-ecdfe5b3a960
+
+import UIKit
+
+class ActivityView: UIView {
+
+    @IBOutlet var contentView: UIView!
+    @IBOutlet weak var testLabel: UILabel!
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    private func commonInit() {
+        
+        Bundle.main.loadNibNamed("ActivityView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.frame = self.bounds
+        contentView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
+        
+    }
+}
