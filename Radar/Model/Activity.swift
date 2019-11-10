3a7f683a9bf98bc186c3f3428ab4882eabdd4327
Charel Felten <charel.felten@gmail.com>
2019-11-10 12:20:48 +0100

    model now nearly finished

--- Radar/Model/Activity.swift
+++ Radar/Model/Activity.swift
@@ -9,86 +9,107 @@
 import Foundation
 import MapKit
 
-class ActivityAnnotation
-
-// activity has to be a class because of the limitations and inheritance, but maybe one way of doing it is to create a new class called ActivityAnnotation that
-// contains an Activity? not sure...
-class Activity: NSObject, MKAnnotation {
-    
-    // activity adopts to these two protocols for it to be displayable on the map
-    // this requires the following properties: title, subtitle, coordinate
+class ActivityWrapper: NSObject, MKAnnotation {
     let coordinate: CLLocationCoordinate2D
-    var title: String?
-    var subtitle: String?
-
+    let activity: Activity
+    let title: String?
+    let subtitle: String?
     
-    let name: String
-    let desc: String
-    let emoji: String
-    let domain: String
-    let type: String
-    
-    let creationTime: Date
-    let activityTime: Date
-    
-    let id: UUID
-    
-    var comments: [(UUID, String, String)]
-    
-    
-    
-    
-    
-    init(name: String,
-         desc: String,
-         domain: String,
-         type: String,
-         coordinate: CLLocationCoordinate2D,
-         activityTime: Date) {
-        
-        //super.init() //not sure if necessary
-        
-        self.name = name
-        self.desc = desc
-        self.domain = domain
-        self.type = type
-        self.emoji = emojiDictionary[self.domain]?[self.type] ?? "❌"
-        
-        self.title = name
-        self.subtitle = desc
-        self.coordinate = coordinate
-        
-        self.id = UUID()
-        
-        self.activityTime = activityTime
-        self.creationTime = Date()
-        
-        self.comments = []
-        
-        
+    init(activity: Activity) {
+        self.activity = activity
+        self.title = activity.name
+        self.coordinate = activity.coordinate
+        self.subtitle = activity.desc
     }
-    
-    
-    
 }
 
 
-//struct Category {
-//    let name: EnumCategory
-//    let subccategories: [Subcategory]
-//}
-//
-//struct Subcategory {
-//    let name: EnumSubcategory
-//    let emoji: Character
-//}
+struct Activity: Identifiable, Codable {
+    
+    let name: String
+    let desc: String
+    
+    let subcategory: Subcategory
+    let category: Category
+    let emoji: String
+    
+    private var customCoordinate: Coordinate
+    var coordinate: CLLocationCoordinate2D {
+        get {
+            return CLLocationCoordinate2D(customCoordinate)
+        }
+        set {
+            customCoordinate = Coordinate(newValue)
+        }
+    }
+    
+    let creationTime: Date = Date()
+    let activityTime: Date
+    
+    let id: UUID = UUID()
+    
+    var comments: [Comment] = []
+    var participants: [User] = []
+    //let creator: User
+    
+    init(name: String, desc: String, subcategory: Subcategory,
+         coordinate: CLLocationCoordinate2D, activityTime: Date) {
+        self.name = name
+        self.desc = desc
+        
+        self.subcategory = subcategory
+        self.category = subcategory.category
+        self.emoji = subcategory.emoji
+        
+        self.customCoordinate = Coordinate(coordinate)
+        
+        self.activityTime = activityTime
+    }
+}
 
-enum Category: String {
+struct Coordinate: Codable, Hashable {
+    // custom coordinate class to make it codable:
+    // https://www.objc.io/blog/2018/10/23/custom-types-for-codable/
+    let lat, long: Double
+    
+    init(_ coordinate: CLLocationCoordinate2D) {
+        lat = coordinate.latitude
+        long = coordinate.longitude
+    }
+}
+
+extension CLLocationCoordinate2D {
+    init(_ coordinate: Coordinate) {
+        self = CLLocationCoordinate2D(latitude: coordinate.lat, longitude: coordinate.long)
+    }
+}
+
+
+
+
+
+struct Comment: Codable {
+    let user: User
+    let content: String
+    let id: String = UUID().uuidString
+}
+
+class User: Codable {
+    //let username: String
+    let id: String = UUID().uuidString
+    
+    private init(){}
+    static let user = User()
+}
+
+
+enum Category: String, Codable {
     case sport = "Sport"
     case game = "Game"
 }
 
-enum Subcategory: String {
+
+enum Subcategory: String, Codable {
     case soccer = "Soccer"
     case basketball = "Basketball"
     case tennis = "Tennis"
@@ -96,24 +117,43 @@ enum Subcategory: String {
     case videogame = "Videogame"
     case boardgame = "Boardgame"
     
+    var category: Category {
+        switch self {
+        case .soccer, .basketball, .tennis:
+            return .sport
+        case .videogame, .boardgame:
+            return .game
+        }
+    }
+    
+    var emoji: String {
+        switch self {
+        case .soccer: return "⚽️"
+        case .basketball: return "🏀"
+        case .tennis: return "🥎"
+        
+        case .videogame: return "🎮"
+        case .boardgame: return "🎲"
+        }
+    }
 }
 
 
-struct AllCategories {
-    let subcategories: [Category:[Subcategory:Character]] = [
-        .sport:[
-            .soccer:"⚽️",
-            .basketball:"🏀",
-            .tennis:"🥎"
-        ],
-        .game:[
-            .videogame: "🎮",
-            .boardgame: "🎲"
-        ]
-    ]
+
+
+
+let testData: [Activity] = [
+    Activity(name: "dungeons and dragons",
+             desc: "lets meet for some dnd games! We're a group of 3 people needing one more motivated person to join so we can play. If you are interested, join the activity and we can discuss details in the description",
+             subcategory: .boardgame,
+             coordinate: CLLocationCoordinate2D(latitude: 49.631622, longitude: 6.171935),
+             activityTime: Date()
+    ),
     
-    
-}
-
-
-
+    Activity(name: "Fussbal match",
+             desc: "fussbal match zu cruchten",
+             subcategory: .soccer,
+             coordinate: CLLocationCoordinate2D(latitude: 49.621622, longitude: 6.161935),
+             activityTime: Date()
+    )
+]
